45005ed664850ae6adaee29cdde8a1d5462b16c6
TharushaKula <tharusha22589@gmail.com>
2024-11-09 18:54:13 +0530

    implement message sending functionality and protect routes with JWT authentication

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 dotenv.config();
 
@@ -9,9 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth",authRoutes)
-// app.use("/api/messages", messageRoutes)
+app.use("/api/messages", messageRoutes)
 
 app.listen(PORT, () => {
   connectToMongoDB();
